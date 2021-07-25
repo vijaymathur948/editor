5eaad95963e5ae86d9c3506cda9307de7ac91557
vijaymathur2025 <vijaymathur2020.2025@gmail.com>
2021-07-25 16:42:30 +0530

    we can count characters while loading document

--- src/App.js
+++ src/App.js
@@ -6,7 +6,6 @@ import {
   Toolbar,
 } from "@syncfusion/ej2-react-documenteditor"
 import "./App.css"
-import ramraj from "./assets/ramraj.doc"
 
 DocumentEditorContainerComponent.Inject(Toolbar)
 export default class App extends React.Component {
@@ -19,10 +18,41 @@ export default class App extends React.Component {
   setData(data) {
     this.setState({ data: data })
   }
-
+  componentDidMount() {
+    this.hidePropertiesPane()
+  }
   loadDcoument = () => {
     console.log("data", this.state.data)
     this.container.documentEditor.open(this.state.data)
+    this.onContentChange()
+  }
+  onContentChange() {
+    const data = JSON.parse(this.container.documentEditor.serialize())
+    const sections = data.sections[0].blocks
+    var total = 0
+    console.clear()
+    sections.map(obj_1 => {
+      obj_1.inlines.map(obj_2 => {
+        var text = obj_2.text
+        text = text.replaceAll(" ", "")
+        total += text.length
+        console.log("text", obj_2.text)
+      })
+    })
+    console.log(
+      "text",
+      data,
+      "lines",
+      sections.length,
+      "calculated lines  : ",
+      Number(total / 60),
+      "characters: ",
+      total,
+      ""
+    )
+  }
+  hidePropertiesPane() {
+    this.container.showPropertiesPane = false
   }
   render() {
     return (
@@ -31,60 +61,44 @@ export default class App extends React.Component {
           type='file'
           onChange={e => {
             var file
-            if (e.target.files[0]) file = e.target.files[0]
-            console.log("files", file)
+            if (e.target.files[0]) {
+              file = e.target.files[0]
+              console.log("files", file)
 
-            var f = new FormData()
-            f.append("file", file)
+              var f = new FormData()
+              f.append("file", file)
 
-            axios
-              .post(
-                "https://ej2services.syncfusion.com/production/web-services/api/documenteditor/Import",
-                f
-              )
-              .then(res => {
-                console.log("response", res)
-                this.setData(res.data)
-                setTimeout(() => this.loadDcoument(), 1000)
-              })
-              .catch(err => console.log("error", err))
+              axios
+                .post(
+                  "https://ej2services.syncfusion.com/production/web-services/api/documenteditor/Import",
+                  f
+                )
+                .then(res => {
+                  console.log("response", res)
+                  this.setData(res.data)
+                  setTimeout(() => this.loadDcoument(), 1000)
+                })
+                .catch(err => console.log("error", err))
+            }
           }}
         />
-
-        <DocumentEditorContainerComponent
-          contentChange={e => {
-            const data = JSON.parse(this.container.documentEditor.serialize())
-            const sections = data.sections[0].blocks
-            var total = 0
-            console.clear()
-            sections.map(obj_1 => {
-              obj_1.inlines.map(obj_2 => {
-                var text = obj_2.text
-                text = text.replaceAll(" ", "")
-                total += text.length
-                console.log("text", obj_2.text)
-              })
-            })
-            console.log(
-              "text",
-              data,
-              "lines",
-              sections.length,
-              "calculated lines  : ",
-              Number(total / 60),
-              "characters: ",
-              total,
-              ""
-            )
-          }}
-          ref={scope => {
-            this.container = scope
-          }}
-          id='container'
-          height='96vh'
-          enableToolbar={true}
-          serviceUrl='https://ej2services.syncfusion.com/production/web-services/api/documenteditor/'
-        />
+        <form action='#'>
+          <div style={{ width: "auto", height: "auto" }} name='textarea'>
+            <DocumentEditorContainerComponent
+              contentChange={e => this.onContentChange(e)}
+              ref={scope => {
+                this.container = scope
+              }}
+              id='container'
+              height='50vh'
+              enableToolbar={true}
+              serviceUrl='https://ej2services.syncfusion.com/production/web-services/api/documenteditor/'
+            />
+          </div>
+          <br />
+          <br />
+          <input type='submit' name='Submit' />
+        </form>
       </>
     )
   }
